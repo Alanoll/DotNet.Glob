052003306c87bdd9d019625c670367cf89c410c5
Alan Collins <alanollv@gmail.com>
2017-08-23 19:38:27 -0500

    Adding NET35 Framework Target (for us unlucky few)

---
+++ src/DotNet.Glob/Extensions/StringBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Text
+{
+    internal static class StringBuilderExtensions
+    {
+#if NET35
+        public static void Clear(this StringBuilder stringBuilder)
+        {
+            stringBuilder.Remove(0, stringBuilder.Length);
+        }
+#endif
+    }
+}
